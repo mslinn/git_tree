a03cedf87c59880da609a47963311406256a9c69
Mike Slinn <mslinn@micronauticsresearch.com>
2023-05-01 12:07:36 -0400

    -

--- bin/make_test_directory.rb
+++ bin/make_test_directory.rb
@@ -3,21 +3,11 @@ require 'fileutils'
 ROOT = 'demo'
 
 def make_repo(path)
-  git_dir = "#{ROOT}/#{path}/.git"
-  FileUtils.mkdir_p git_dir
-  FileUtils.touch "#{git_dir}/.gitkeep"
   basename = File.basename path
-  content = <<~END_CONFIG
-    [core]
-        repositoryformatversion = 0
-        filemode = true
-        bare = false
-        logallrefupdates = true
-    [remote "origin"]
-        url = git@github.com:mslinn/#{basename}.git
-        fetch = +refs/heads/*:refs/remotes/origin/*
-  END_CONFIG
-  File.write "#{ROOT}/#{path}/.git/config", content
+  git_dir = "#{ROOT}/#{path}"
+  FileUtils.mkdir_p git_dir
+  repo = Rugged::Repository.init_at git_dir
+  repo.remotes.create 'origin', "git@github.com:mslinn/#{basename}.git"
 end
 
 %w[a b c].each do |x|
