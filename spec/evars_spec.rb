dfd85fd34f23bd6526ecfc65d5f3c12f422c4a4d
Mike Slinn <mslinn@mslinn.com>
2023-05-05 10:41:00 -0400

    -

--- spec/evars_spec.rb
+++ spec/evars_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/evar'
 
 RSpec.describe(Evars) do
-  it 'makes env vars' do
+  it 'finds common prefix' do
     strings = [
       '/root/sub1/sub2/blah',
       '/root/sub1/sub2',
@@ -10,5 +10,16 @@ RSpec.describe(Evars) do
     ]
     actual = described_class.common_prefix strings
     expect(actual).to eq '/root'
+  end
+
+  it 'finds common prefix again' do
+    strings = [
+      '/root/sub1/sub2/blah',
+      '/root/sub1/sub2',
+      '/root/sub1',
+      '/root2/sub3/sub1'
+    ]
+    actual = described_class.common_prefix strings
+    expect(actual).to eq '/'
   end
 end
