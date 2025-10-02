b164b0891029dededbeff42f7ee4c92b693a88dd
Mike Slinn <mslinn@mslinn.com>
2025-10-02 17:36:14 -0400

    -

--- lib/util/thread_pool_manager.rb
+++ lib/util/thread_pool_manager.rb
@@ -6,18 +6,6 @@ class FixedThreadPoolManager
 
   SHUTDOWN_SIGNAL = :shutdown
 
-  # Constructor/dispatcher for new FixedThreadPoolManager instances
-  # See playground/process_messages.rb for examples of how to use this
-  # @param task_input_messages [Array[String]] The contents of the array will be parceled out to
-  #   individual threads as they become available.
-  # @return void Note the thread pool is torn down and the program exits
-  #   shortly after this method returns.
-  def self.dispatch_work(task_input_messages, &)
-    pool = FixedThreadPoolManager.new
-    pool.create_tasks_from(task_input_messages)
-    pool.run(&)
-  end
-
   # Calculate the number of worker threads as 75% of available processors
   # (less one for the monitor thread), with a minimum of 1.
   def initialize(percent_available_processors = 0.75)
@@ -29,26 +17,15 @@ class FixedThreadPoolManager
       log_stderr msg, :red
       exit! 1
     end
-    @worker_count = [((Etc.nprocessors - 1) * percent_available_processors).floor, 1].max
+    @worker_count = [(Etc.nprocessors * percent_available_processors).floor, 1].max
     @main_work_queue = Queue.new
     @workers = []
-  end
-
-  # Task producer: initiates tasks by sending messages to the monitor thread.
-  def create_tasks_from(input_messages)
-    log_stderr "[Producer] Creating #{input_messages.count} tasks..."
-    input_messages.each { |msg| @main_work_queue.push msg }
-    @main_work_queue.push(SHUTDOWN_SIGNAL) # Signal that production is complete.
   end
 
   # Adds a single task to the work queue.
   # The pool must have been started with `start` first.
   def add_task(task)
     @main_work_queue.push(task)
-  end
-
-  def max_worker_count
-    @worker_count
   end
 
   # A thread-safe output method for colored text.
@@ -65,16 +42,6 @@ class FixedThreadPoolManager
   def log_stdout(multiline_string)
     $stdout.puts multiline_string
     $stdout.flush
-  end
-
-  # Starts the workers and the monitor, then waits for everything to complete.
-  # It accepts a block that defines what work each worker will perform.
-  def run(&)
-    initialize_workers(&)
-    monitor = create_monitor
-
-    monitor.join # Wait for the monitor to finish (which in turn waits on all workers).
-    log_stderr "\nAll work is complete.", :green
   end
 
   # Signals the pool to shut down after all currently queued tasks are processed.
@@ -94,7 +61,6 @@ class FixedThreadPoolManager
   # @return nil
   def start(&)
     initialize_workers(&)
-    @monitor = create_monitor
   end
 
   # This is the last method to call when using FixedPoolManager.
@@ -102,40 +68,17 @@ class FixedThreadPoolManager
   # It pauses the execution of your main thread and waits until the monitor and all worker threads have
   # fully completed their work and terminated.
   def wait_for_completion
-    @monitor&.join
+    # Signal all workers to shut down
+    @worker_count.times do
+      @main_work_queue.push(SHUTDOWN_SIGNAL)
+    end
+
+    # Wait for all worker threads to finish
+    @workers.each(&:join)
+    log_stderr "\nAll work is complete.", :green
   end
 
   private
-
-  # Creates a monitor thread that manages the thread pool.
-  # It takes tasks from the work queue and dispatches them to worker threads.
-  def create_monitor
-    Thread.new do
-      log_stderr "[Monitor] Ready to dispatch work."
-      worker_index = 0
-      loop do
-        # The monitor blocks here, waiting for the producer to send a task.
-        task = @main_work_queue.pop
-        break if task == SHUTDOWN_SIGNAL
-
-        # Distribute the task to the next worker in a round-robin fashion.
-        target_worker = @workers[worker_index]
-        log_stderr "[Monitor] Dispatching '#{task}' to Worker #{worker_index}."
-        target_worker[:queue].push(task)
-
-        # Move to the next worker for the next task.
-        worker_index = (worker_index + 1) % @worker_count
-      end
-
-      log_stderr "[Monitor] Received shutdown signal. Relaying to all workers...", :yellow
-      # Wait for all workers to finish.
-      @workers.each do |worker|
-        worker[:queue].push(SHUTDOWN_SIGNAL)
-        worker[:thread].join
-      end
-      log_stderr "[Monitor] All workers have shut down. Monitor finished.", :yellow
-    end
-  end
 
   # @param Block of code to execute for each task.
   def initialize_workers
@@ -143,15 +86,16 @@ class FixedThreadPoolManager
     @worker_count.times do |i|
       worker_queue = Queue.new
       worker_thread = Thread.new do
+        log_stderr "  [Worker #{i}] Started.", :cyan
         loop do
-          task = worker_queue.pop # The worker blocks here, waiting for the monitor to give it a task.
+          task = @main_work_queue.pop # The worker blocks here, waiting for a task.
           break if task == SHUTDOWN_SIGNAL
 
           yield(self, task, i) # Execute the provided block of work.
         end
         log_stderr "  [Worker #{i}] Shutting down.", :cyan
       end
-      @workers << { thread: worker_thread, queue: worker_queue }
+      @workers << worker_thread
     end
   end
 end
